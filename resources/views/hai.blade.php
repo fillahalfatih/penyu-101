7dc7bac98c844bd0741ee2a8c2db150add38787e
Muhammad Fillah Alfatih <fillahalfatih@users.noreply.github.com>
2025-02-12 23:43:24 +0700

    add feature #2

---
+++ resources/views/hai.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Hai Page</title>
+</head>
+<body>
+    <h1>Welcome to the Hai Page!</h1>
+    <p>This page is designed to greet visitors.</p>
+    <footer>
+        <p>&copy; <?php echo date("Y"); ?> Your Company. All rights reserved.</p>
+    </footer>
+</body>
+</html>
